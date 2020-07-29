c512d9d18f51567f6de7d75c732679b0b8678067
Nichapakawadee2020 <68819150+Nichapakawadee2020@users.noreply.github.com>
2020-07-29 15:46:33 +0700

    Add files via upload

---
+++ HomeWorkChapter2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout<<endl;
+   cout << "1st Term of 2020\t\t\\590602140125\tNichapakawadee Prommajun "<<endl;
+   cout << "\n\n---------------------------------------------------------------------------------------------"<<endl;
+   cout << "\n| Subject ID | Subject name\t\t|  Score (100)  | Grade | "<<endl; 
+   cout << "\n---------------------------------------------------------------------------------------------"<<endl;
+   cout << "\n| 4570034    | Introduction to computer |\t85\t|  \"A\"  |"<<endl;
+   cout << "\n| 4582047    | English    \t\t|\t74\t|  \"B\"  |"<<endl;
+   cout << "\n| 4582047    | Design Thinking\t\t|\t92\t|  \"A\"  |"<<endl;
+   cout << "\n---------------------------------------------------------------------------------------------"<<endl;
+   cout << "\n|   TOTAL    | Subject = 3\t\t| Average = 3.87 | "<<endl; 
+   cout << "\n============================================================================================="<<endl;
+   cout << "\n\n\nKMUTNB " <<endl;
+
+   return(0);
+
+}
+
